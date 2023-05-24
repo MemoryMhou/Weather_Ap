8d96e9f2c264c9a4ea8382647ca2e2fe88cf3dc3
MemoryMhou <mhoumemory1@gmail.com>
2023-05-24 21:14:27 +0200

    Initial Commit

---
+++ Weather_Ap/Weather_ApApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ApApp.swift
+//  Weather_Ap
+//
+//  Created by Memory Mhou on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ApApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
